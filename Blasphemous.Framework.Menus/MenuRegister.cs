75b5d6be295c2513aa779bf0fb51241cf3600fd6
unknown <brandenek03912@hotmail.com>
2024-02-28 12:08:22 -0500

    core: add register and base menu

---
+++ Blasphemous.Framework.Menus/MenuRegister.cs
@@ -0,0 +1,35 @@
+﻿using Blasphemous.ModdingAPI;
+using System.Collections.Generic;
+
+namespace Blasphemous.Framework.Menus;
+
+/// <summary> Registers a new menu </summary>
+public static class MenuRegister
+{
+    private static readonly List<ModMenu> _newMenus = new();
+    private static readonly List<ModMenu> _loadMenus = new();
+
+    internal static IEnumerable<ModMenu> NewGameMenus => _newMenus;
+    internal static IEnumerable<ModMenu> LoadGameMenus => _loadMenus;
+
+    /// <summary> Registers a menu to appear before starting a new game </summary>
+    public static void RegisterNewGameMenu(this ModServiceProvider provider, ModMenu menu)
+    {
+        if (provider == null)
+            return;
+
+        _newMenus.Add(menu);
+        Main.MenuFramework.Log($"Registered new game menu: {menu.GetType().Name}");
+    }
+
+    /// <summary> Registers a menu to appear before loading an existing game </summary>
+    public static void RegisterLoadGameMenu(this ModServiceProvider provider, ModMenu menu)
+    {
+        if (provider == null)
+            return;
+
+        _loadMenus.Add(menu);
+        Main.MenuFramework.Log($"Registered load game menu: {menu.GetType().Name}");
+
+    }
+}
---
+++ Blasphemous.Framework.Menus/ModMenu.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+
+namespace Blasphemous.Framework.Menus;
+
+public abstract class ModMenu(string title, int priority)
+{
+    internal string Title { get; } = title;
+    internal int Priority { get; } = priority;
+
+    /// <summary>
+    /// Called when the menus are first opened
+    /// </summary>
+    public virtual void OnStart() { }
+
+    /// <summary>
+    /// Called when the menus are closed by starting the game
+    /// </summary>
+    public virtual void OnFinish() { }
+
+    /// <summary>
+    /// Called when the menus are closed by returning to the title screen
+    /// </summary>
+    public virtual void OnCancel() { }
+
+    /// <summary>
+    /// Called when this specific menu is opened
+    /// </summary>
+    public virtual void OnShow() { }
+
+    /// <summary>
+    /// Called when this specific menu is closed
+    /// </summary>
+    public virtual void OnHide() { }
+
+    ///// <summary>
+    ///// Creates the specific UI for this menu if it doesnt already exist
+    ///// </summary>
+    //internal void CreateUI(bool isFirst, bool isLast)
+    //{
+    //    if (UI != null)
+    //        return;
+
+    //    UI = MenuModder.CreateBaseMenu(OwnerMod, Title, isFirst, isLast);
+    //    CreateUI(UI.transform.Find("Main Section"));
+    //}
+
+    ///// <summary>
+    ///// Adds all menu-specific UI to its base UI object
+    ///// </summary>
+    //protected internal abstract void CreateUI(Transform ui);
+
+    ///// <summary>
+    ///// Adds an event to occur whenever this object is clicked on
+    ///// </summary>
+    //protected void AddClickable(RectTransform rect, Action onClick) => UI.AddClickable(rect, onClick, null);
+
+    ///// <summary>
+    ///// Adds an event to occur whenever this object is clicked on or clicked off
+    ///// </summary>
+    //protected void AddClickable(RectTransform rect, Action onClick, Action onUnclick) => UI.AddClickable(rect, onClick, onUnclick);
+}
