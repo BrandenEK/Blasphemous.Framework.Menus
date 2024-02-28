08d7c89d9787e460457e953abdd3c393831fa806
unknown <brandenek03912@hotmail.com>
2024-02-28 12:15:00 -0500

    tests: add test menus

--- Blasphemous.Framework.Menus/MenuFramework.cs
+++ Blasphemous.Framework.Menus/MenuFramework.cs
@@ -6,8 +6,13 @@ public class MenuFramework : BlasMod
 {
     public MenuFramework() : base(ModInfo.MOD_ID, ModInfo.MOD_NAME, ModInfo.MOD_AUTHOR, ModInfo.MOD_VERSION) { }
 
-    protected override void OnInitialize()
+#if DEBUG
+    protected override void OnRegisterServices(ModServiceProvider provider)
     {
-        LogError($"{ModInfo.MOD_NAME} has been initialized");
+        provider.RegisterNewGameMenu(new TestMenu("Testing number 1", 10));
+        provider.RegisterNewGameMenu(new TestMenu("Testing number 2", 21));
+
+        provider.RegisterLoadGameMenu(new TestMenu("Loading game...", 50));
     }
+#endif
 }
---
+++ Blasphemous.Framework.Menus/TestMenu.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Blasphemous.Framework.Menus;
+
+public class TestMenu(string title, int priority) : ModMenu(title, priority)
+{
+    public override void OnShow()
+    {
+        Main.MenuFramework.LogWarning($"Showing {Title} menu");
+    }
+
+    public override void OnHide()
+    {
+        Main.MenuFramework.LogWarning($"Hiding {Title} menu");
+    }
+}
